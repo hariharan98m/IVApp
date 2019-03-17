e8aa12ca841a1525dad81d39c219a17292e62225
Pranilak <aneeshprabu@gmail.com>
2019-03-17 17:49:38 +0530

    centered homepage and added card Aneesh

--- src/components/team.vue
+++ src/components/team.vue
@@ -20,10 +20,11 @@
 									<h1 class="title is-size-4 has-text-centered is-light">
 										Hariharan
 									</h1>
-									<p class="is-size-5">
+									<p class="is-size-5 is-size-6-mobile">
 										An explorer, fascinated by and infatuated with AI, who's crazy about creating new stuff, just for the fun in it.
                                         A learner of people, life and things, he's into anything born out of creative thought, offering up new possibility and ticking his 'FASCINATION' clock to eternity.
 									</p>	
+                  <br>
 								</div>
 						</article>
 
@@ -39,7 +40,7 @@
 									<h1 class="title is-size-4 has-text-centered is-light">
 										Prof. Balaji 
 									</h1>
-									<p class="is-size-5">
+									<p class="is-size-5 is-size-6-mobile">
 										A business mastermind, who believes his philosophy of strategic marketing, taking user experience to the next horizon and the level beyond. 
                                         A virtuous, ever motivating and a contemplating soul, who's a bringer of disruptive product ideas, born out of passion and field experience. 
 									</p>	
@@ -47,6 +48,26 @@
 						</article>
 
 					</div><!--End of Column-->
+
+
+          <div class="column">
+						<article class="notification">
+                            <figure class="image is-128x128 has-image-centered">
+                                        <img class="is-rounded" src="../images/Aneesh.jpg">
+                            </figure>
+                            <br>
+								<div class="content">
+									<h1 class="title is-size-4 has-text-centered is-light">
+										P.Aneesh
+									</h1>
+									<p class="is-size-5 is-size-6-mobile">
+										An avid learner, A source of motivation. Interested in various technologies and advancements including the ever popular AI and deeplearning. Working towards a better tomorrow.
+									</p>	
+                  <br><br><br><br>
+								</div>
+						</article>
+
+            </div><!--End of Column-->
 
 
 				</div>
